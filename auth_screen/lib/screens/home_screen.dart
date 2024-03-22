aded9e8158f54792a2a719dc00689034f0035c9e
brahmee07 <113072666+brahmee07@users.noreply.github.com>
2024-03-21 19:52:54 -0500

    fixed the profile tab

--- auth_screen/lib/screens/home_screen.dart
+++ auth_screen/lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
-import 'package:auth_screen/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:auth_screen/screens/profile_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,37 +11,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0; // track the selected index
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HOME'),
+        title: Text(_getSelectedTitle()),
+        automaticallyImplyLeading: _selectedIndex == 2 ? false : true, // Hide back button for Profile screen
       ),
-
+      body: _buildBody(),
       //bottom nav bar
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
-          BottomNavigationBarItem(label: "Game",icon: Icon(Icons.gamepad),),
+          BottomNavigationBarItem(label: "Game", icon: Icon(Icons.gamepad)),
           BottomNavigationBarItem(label: "Profile", icon: Icon(Icons.person))
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
-        onTap: (int index){
+        onTap: (int index) {
           setState(() {
             _selectedIndex = index;
-            if (index == 2) {
-               // Check if the "Profile" button is tapped
-              Navigator.push(
-
-                context,
-                MaterialPageRoute(builder: (context) => ProfileScreen()),
-              );}
-              
           });
         },
       ),
+    );
+  }
+
+  String _getSelectedTitle() {
+    switch (_selectedIndex) {
+      case 0:
+        return "Home";
+      case 1:
+        return "Game";
+      case 2:
+        return "Profile";
+      default:
+        return "Home";
+    }
+  }
+
+  Widget _buildBody() {
+    switch (_selectedIndex) {
+      case 0:
+        return _buildHomePage();
+      case 1:
+        return _buildGamePage();
+      case 2:
+        return ProfileScreen();
+      default:
+        return Container();
+    }
+  }
+
+  Widget _buildHomePage() {
+    return Center(
+      child: Text('Home Page'),
+    );
+  }
+
+  Widget _buildGamePage() {
+    return Center(
+      child: Text('Game Page'),
     );
   }
 }
