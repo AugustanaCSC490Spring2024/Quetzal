18e82950626edb568661378a702811aaca7cf1fc
Rijan <rijankafle20@augustana.edu>
2024-03-27 15:01:31 -0500

    cleaned up home page

--- auth_screen/lib/screens/home_screen.dart
+++ auth_screen/lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
+import 'package:auth_screen/search_button.dart';
 import 'package:flutter/material.dart';
 import 'package:auth_screen/screens/profile_screen.dart';
 import 'package:auth_screen/api_service.dart';
-import 'package:auth_screen/search_button.dart'; // Import the SearchButton class
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -80,60 +80,13 @@ class _HomePageState extends State<HomePage> {
           : SingleChildScrollView(
               child: Column(
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Button pressed');
-                      _fetchMarketValues();
-                    },
-                    child: Text('Fetch Ticker & Name'),
-                  ),
-                  SizedBox(height: 20),
-                  _marketValues.isNotEmpty
-                      ? ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: _marketValues.length,
-                          itemBuilder: (context, index) {
-                            final stock = _marketValues[index];
-                            return ListTile(
-                              title: Text(stock['ticker']),
-                              subtitle: Text(stock['name']),
-                            );
-                          },
-                        )
-                      : SizedBox(),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _marketValues.clear();
-                      });
-                    },
-                    child: Text('Done'),
-                  ),
+ 
                 ],
               ),
             ),
     );
   }
 
-  Future<void> _fetchMarketValues() async {
-    setState(() {
-      _isLoading = true;
-    });
-    try {
-      final List<Map<String, dynamic>> marketValues =
-          await APIService().fetchStockMarketValues();
-      setState(() {
-        _marketValues = marketValues;
-        _isLoading = false;
-      });
-    } catch (e) {
-      print('Error fetching market values: $e');
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
 
   Widget _buildGamePage() {
     return Center(
