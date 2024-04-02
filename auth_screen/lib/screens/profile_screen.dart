de1e93ac1ca1bb7fb2b2da8607eb2bdff4853315
brahmee07 <113072666+brahmee07@users.noreply.github.com>
2024-04-02 09:12:29 -0500

    fixed another bug

--- auth_screen/lib/screens/profile_screen.dart
+++ auth_screen/lib/screens/profile_screen.dart
@@ -1,4 +1,4 @@
-import 'package:auth_screen/screens/home_screen.dart';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:auth_screen/screens/sign_in_screen.dart';
@@ -62,7 +62,7 @@ class ProfileScreen extends StatelessWidget {
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(
-                      builder: (BuildContext context) => const HomePage(),
+                      builder: (BuildContext context) => const SignIn(),
                     ),
                   );
                 });
