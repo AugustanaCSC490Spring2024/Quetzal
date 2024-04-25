ad077077741025a9aeb92c0bf2eba551a73092ba
brahmee07 <113072666+brahmee07@users.noreply.github.com>
2024-04-24 23:09:47 -0500

    added ticker news in the detail page

--- auth_screen/lib/api_service.dart
+++ auth_screen/lib/api_service.dart
@@ -1,32 +1,31 @@
-// ignore_for_file: avoid_print
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class APIService {
-  final String apiKey = "hDnp3QGn94ARKy0B8mzeEQyX9qY_Bwym";
+class ApiService {
+  static Future<List<String>> fetchTickerNews(String stockSymbol) async {
+    const apiKey = 'hDnp3QGn94ARKy0B8mzeEQyX9qY_Bwym';
+    const baseUrl = 'https://api.polygon.io/v2/reference/news';
+    final url = '$baseUrl?apiKey=$apiKey&ticker=$stockSymbol';
 
-  Future<Map<String, dynamic>> fetchTickerInfo(String ticker) async {
     try {
-      final DateTime currentDate = DateTime.now();
-      final String formattedDate =
-          '${currentDate.year}-${currentDate.month.toString().padLeft(2, '0')}-${currentDate.day.toString().padLeft(2, '0')}';
-      
-      final response = await http.get(
-        Uri.parse(
-          'https://api.polygon.io/v3/reference/tickers/$ticker?date=$formattedDate&apiKey=$apiKey'),
-      );
+      final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> data = jsonDecode(response.body);
-        return data['results'];
+        final jsonData = jsonDecode(response.body);
+        List<String> newsHeadlines = [];
+
+        for (var article in jsonData['results']) {
+          newsHeadlines.add(article['title']);
+        }
+
+        return newsHeadlines;
       } else {
-        print('Failed to fetch ticker info: ${response.statusCode}');
-        throw Exception('Failed to fetch ticker info');
+        throw Exception('Failed to load news');
       }
     } catch (e) {
-      print('Error fetching ticker info: $e');
-      throw Exception('Error fetching ticker info: $e');
+      throw Exception('Error: $e');
     }
   }
+
+ 
 }
