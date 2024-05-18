5293506655f4e1ba516f941d2e4f6b0b0f96bf69
Yazid Mouayn <yazidmouayn21@augustana.edu>
2024-05-18 09:41:03 -0500

    MARKETSIM have now a Splash Screen

---
+++ auth_screen/lib/screens/splashscreen.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:auth_screen/screens/sign_in_screen.dart';  // Your main sign-in screen
+
+class DynamicSplashScreen extends StatefulWidget {
+  const DynamicSplashScreen({super.key});
+
+  @override
+  State<DynamicSplashScreen> createState() => _DynamicSplashScreenState();
+}
+
+class _DynamicSplashScreenState extends State<DynamicSplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1), 
+      vsync: this,
+    );
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    _controller.forward().then((_) {
+      Future.delayed(const Duration(seconds: 2), () {
+        _controller.reverse();
+      });
+    });
+
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    await Future.delayed(const Duration(seconds: 5));  // Adjusted for fade animation
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const SignIn()));  // Navigate to the SignIn screen
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            const Text(
+              'MARKETSIM',
+              style: TextStyle(
+                fontSize: 32,  // Larger font size
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                letterSpacing: 2,  // Spacing between letters
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            Image.asset("assets/icon/MARKETSIM1024_1024.png", width: 180),
+            FadeTransition(
+              opacity: _fadeAnimation,
+              child: const Text(
+                'Trade Smart',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            ),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const SizedBox(height: 10),
+            const Text('Loading...', style: TextStyle(color: Colors.grey)),
+          ],
+        ),
+      ),
+    );
+  }
+}
